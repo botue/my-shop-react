a349de99ec2a36dbfa0c8871ddeb4904067d1fc0
botue <botue@sina.com>
2022-01-19 11:33:05 +0800

    feat: 搭建首页基础布局

--- src/containers/Index/index.tsx
+++ src/containers/Index/index.tsx
@@ -1,3 +1,37 @@
+import styles from "./index.module.scss";
+
 export default function Index(): JSX.Element {
-  return <h3>首页</h3>;
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.message}>
+          <span className={styles.lead}>今天，</span>
+          2022年01月07日周五，欢迎回到后台操作系统，累计售出59件商品，继续加油~
+        </div>
+        <div className={styles.todos}>
+          <a href="#!" className={styles.todo}>
+            <strong>25</strong>
+            <span>待入库</span>
+          </a>
+          <a href="#!" className={styles.todo}>
+            <strong>17</strong>
+            <span>待入库</span>
+          </a>
+          <a href="#!" className={styles.todo}>
+            <strong>928.00</strong>
+            <span>待入库</span>
+          </a>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.overview}>
+          <section className={styles.users}></section>
+          <section className={styles.profit}></section>
+          <section className={styles.orders}></section>
+        </div>
+        <div className={styles.realtime}></div>
+        <div className={styles.goods}></div>
+      </main>
+    </>
+  );
 }
