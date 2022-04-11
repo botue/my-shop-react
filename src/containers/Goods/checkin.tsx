7e6e9e64a531b8bc8a34a8ace0a181b97908fb68
botue <botue@sina.com>
2022-04-11 18:14:59 +0800

    feat: 完善商品入库部分布局

--- src/containers/Goods/checkin.tsx
+++ src/containers/Goods/checkin.tsx
@@ -7,6 +7,260 @@ export default function Checkin(): JSX.Element {
   return (
     <>
       <Header />
+      <div className={styles.goods}>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_7.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_8.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_9.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_10.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_11.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_12.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_13.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_14.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_15.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_16.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_17.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_18.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_19.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+        <div className={styles.item}>
+          <img
+            src={require('../../assets/uploads/goods_20.jpg')}
+            className={styles.cover}
+            alt=""
+          />
+          <p className={styles.price}>
+            ￥82.00 <small>预计收入2.80元</small>
+          </p>
+          <div className={styles.extra}>
+            <p className={styles.name}>
+              木林森男鞋夏季2019新款韩版潮流网布透气鞋
+            </p>
+            <span className={styles.stock}>库存: 18562件</span>
+            <span className={styles.sales}>月销: 6820件</span>
+            <span className={styles.mark}>标注</span>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
