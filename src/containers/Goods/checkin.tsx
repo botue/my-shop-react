702baeec32c9d46c321c3f001c88dca60dd9600a
botue <botue@sina.com>
2022-04-14 17:34:08 +0800

    feat: 提取分页组件

--- src/containers/Goods/checkin.tsx
+++ src/containers/Goods/checkin.tsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import Pagnation from '@/components/Pagnation';
 
 // 样式表
 import styles from './checkin.module.scss';
@@ -282,28 +283,7 @@ export default function Checkin(): JSX.Element {
           </div>
         </div>
       </div>
-
-      {/* 分页 */}
-      <div className={styles.pagnation}>
-        <a href="#!" className={styles.button}>
-          上一页
-        </a>
-        <a href="#">1</a>
-        <a href="#">2</a>
-        <a className={styles.active} href="#">
-          3
-        </a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
-        <a href="#">7</a>
-        <a href="#">8</a>
-        <a href="#">9</a>
-        <a href="#">10</a>
-        <a href="#!" className={styles.button}>
-          下一页
-        </a>
-      </div>
+      <Pagnation />
     </>
   );
 }
