f7917ea382fa41872fd4db3e1ef004ef7426c3e8
botue <botue@sina.com>
2022-04-26 23:17:15 +0800

    feat: 完成商品分类列表布局

--- src/containers/Goods/category.tsx
+++ src/containers/Goods/category.tsx
@@ -1,22 +1,122 @@
-import { Button } from 'antd';
+import { Button, Table, Tag } from 'antd';
 
-import { PlusCircleOutlined, StopOutlined } from '@ant-design/icons';
+// 样式表
+import styles from './category.module.scss';
+
+import {
+  PlusCircleFilled,
+  StopOutlined,
+  CloseCircleFilled,
+  ExclamationCircleFilled,
+} from '@ant-design/icons';
 
 export default function Category(): JSX.Element {
+  const columns = [
+    {
+      title: '分类名称',
+      dataIndex: 'name',
+      key: 'name',
+      width: 200,
+    },
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: 200,
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      key: 'status',
+      render(text: string) {
+        if (!text) return null;
+        return (
+          <Tag color={text === '已禁用' ? '#ff4d4f' : '#ffc306'}>{text}</Tag>
+        );
+      },
+    },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      name: '女装',
+      id: 1,
+      children: [
+        {
+          key: 11,
+          name: '连衣裙',
+          id: 11,
+          status: '已启用',
+        },
+        {
+          key: 12,
+          name: '半身裙',
+          id: 12,
+          status: '已启用',
+        },
+        {
+          key: 13,
+          name: 'T恤女',
+          id: 13,
+          status: '已禁用',
+        },
+        {
+          key: 14,
+          name: '防晒衣',
+          id: 14,
+          status: '已启用',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '男装',
+      id: 2,
+      children: [
+        {
+          key: 21,
+          name: '冲锋衣',
+          id: 21,
+          status: '已启用',
+        },
+        {
+          key: 22,
+          name: '直筒裤',
+          id: 22,
+          status: '已禁用',
+        },
+      ],
+    },
+  ];
+
   return (
     <>
-      <Button type="primary" icon={<PlusCircleOutlined />}>
-        新增
-      </Button>
-      <Button type="primary" icon={<StopOutlined />}>
-        启用
-      </Button>
-      <Button type="primary" icon={<PlusCircleOutlined />}>
-        禁用
-      </Button>
-      <Button type="primary" icon={<PlusCircleOutlined />}>
-        删除
-      </Button>
+      <div className={styles.actions}>
+        <Button type="primary" icon={<PlusCircleFilled />}>
+          新增
+        </Button>
+        <Button type="primary" icon={<StopOutlined />}>
+          启用
+        </Button>
+        <Button type="primary" icon={<ExclamationCircleFilled />}>
+          禁用
+        </Button>
+        <Button type="primary" icon={<CloseCircleFilled />}>
+          删除
+        </Button>
+      </div>
+      <div className={styles.categories}>
+        <Table
+          columns={columns}
+          rowSelection={{
+            type: 'checkbox',
+            checkStrictly: false,
+          }}
+          dataSource={data}
+          pagination={false}
+        />
+      </div>
     </>
   );
 }
